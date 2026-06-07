97d2194181430233a6cac6b2ab84403df5bc41dc
Nyramu <nyramu@noreply.codeberg.org>
2026-06-07 15:04:35 +0200

    refactor(zen4): improved derivation

--- modules/packages/zen4.nix
+++ modules/packages/zen4.nix
@@ -1,14 +1,15 @@
 {
   perSystem =
     { pkgs, ... }:
-
+    
     let
       pname = "eden";
       version = "0.2.1";
       
       src = pkgs.fetchurl {
         url = "https://stable.eden-emu.dev/v${version}/Eden-Linux-v${version}-rog-ally-clang-pgo.AppImage";
-        hash = "sha256-Ak3+MH9+W1ObNUm9kkX2txybqpzHRPMdWNMYGFfYX/w=";
+        hash = "sha256-iiH5+NVFEN2vJblCaqBYwAjytpN+ULZ/4Ik/MSDaRYk=";
+        executable = true;
       };
       
       icon = pkgs.fetchurl {
@@ -16,33 +17,31 @@
         hash = "sha256-XrRaJfrcYvw10SO0/hlGKw5///7X3T8Ukg5UMrOFl2k=";
       };
       
-      rawAppImage = pkgs.stdenv.mkDerivation {
-        name = "${pname}-raw-${version}";
-        inherit src;
-        dontUnpack = true;
-        dontStrip = true;
-        dontPatchELF = true;
-        installPhase = ''
-          mkdir -p $out/bin
-          cp $src $out/bin/${pname}-appimage
-          chmod +x $out/bin/${pname}-appimage
-        '';
+      launchScript = pkgs.writeShellScript "${pname}-launch" ''
+        exec ${src} --appimage-extract-and-run "$@"
+      '';
+    
+      desktopItem = pkgs.makeDesktopItem {
+        name = pname;
+        desktopName = "Eden";
+        exec = pname;
+        icon = pname;
+        categories = [ "Game" "Emulator" ];
       };
       
-      launchScript = pkgs.writeShellScript "${pname}-launch" ''
-        exec ${rawAppImage}/bin/${pname}-appimage --appimage-extract-and-run "$@"
+      iconDrv = pkgs.runCommand "${pname}-icon" { } ''
+        mkdir -p $out/share/icons/hicolor/scalable/apps
+        cp ${icon} $out/share/icons/hicolor/scalable/apps/${pname}.svg
       '';
     
       eden-fhs = pkgs.buildFHSEnv {
         name = pname;
         targetPkgs =
           pkgs: with pkgs; [
-            fuse3
             vulkan-loader
             wayland
             libGL
             mesa
-            glibc
             glib
             libx11
             libxext
@@ -64,21 +63,13 @@
     {
       packages.zen4 = pkgs.symlinkJoin {
         name = "${pname}-${version}";
-        paths = [ eden-fhs ];
-        postBuild = ''
-                  mkdir -p $out/share/applications
-                  cat > $out/share/applications/${pname}.desktop << EOF
-          [Desktop Entry]
-          Name=Eden
-          Exec=${pname}
-          Icon=${pname}
-          Type=Application
-          Categories=Game;Emulator;
-          EOF
-                  mkdir -p $out/share/icons/hicolor/scalable/apps
-                  cp ${icon} $out/share/icons/hicolor/scalable/apps/${pname}.svg
-        '';
-      
+        
+        paths = [
+          eden-fhs
+          desktopItem
+          iconDrv
+        ];
+        
         meta = {
           description = "Nintendo Switch emulator (Zen 4 + PGO)";
           homepage = "https://eden-emu.dev/";
